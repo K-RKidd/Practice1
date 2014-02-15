bd1aba6068d7824526ac9080e9fb635b8150a205
K-RKidd <kr248196@muhlenberg.edu>
2014-02-15 10:07:41 -0500

    Initial Commit

---
+++ Practice1/Practice1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Practice1AppDelegate.h
+//  Practice1
+//
+//  Created by Krystle on 2/15/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Practice1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
